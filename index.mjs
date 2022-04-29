c1d8a87ced7b4ea4e5294b819ef73576db2c6be1
inokawa <48897392+inokawa@users.noreply.github.com>
2022-04-29 23:41:14 +0900

    Use yargs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 import JestHasteMap from "jest-haste-map";
 import { cpus } from "os";
-import { dirname, join } from "path";
+import { dirname, join, resolve } from "path";
 import { fileURLToPath } from "url";
+import chalk from "chalk";
+import yargs from "yargs";
 
 const root = join(dirname(fileURLToPath(import.meta.url)), "product");
 
@@ -18,3 +20,13 @@ const hasteMap = new JestHasteMap.default(hasteMapOptions);
 await hasteMap.setupCachePath(hasteMapOptions);
 const { hasteFS, moduleMap } = await hasteMap.build();
 console.log(hasteFS.getAllFiles());
+
+const options = yargs(process.argv).argv;
+const entryPoint = resolve(process.cwd(), options.entryPoint);
+if (!hasteFS.exists(entryPoint)) {
+  throw new Error(
+    "`--entry-point` does not exist. Please provide a path to a valid file."
+  );
+}
+
+console.log(chalk.bold(`❯ Building ${chalk.blue(options.entryPoint)}`));
